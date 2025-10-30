59b6e56e1681a31c3d63ffba2a212a00d502c241
Rendobro <rendofuny@gmail.com>
2025-10-29 21:49:35 -0400

    Create Rendobro.py
    I created some bars, then I integrated such bars into my introduction

---
+++ introductions/Rendobro.py
@@ -0,0 +1,26 @@
+"""
+Feel free to go off of this template to introduce yourself.
+Be creative! You can add more functions or whatever to your hearts content!
+"""
+
+def introduce():
+    print("Hello! My name is Arian. I'm so extremely and utterly excited to be part of the micromouse community!")
+    print("I love robotics and programming!! I'm also looking forward to learning and collaborating with all of you!")
+    print("Here's a couple bars I wrote")
+    print("""Guess who just made they own gizmo\n
+            Making things because i am the wiz bro
+            Tings so easy to make, 'tis the biz holmes
+            Iciest veins, it bubbles like the fizz cold
+            East of the mississippi they say \"Thats his, yo\"
+            On the west they say my brain is like a bistro
+            Beat me in a fight? Have you seen my fist though
+            You ain't in the know, hit an all time gist low
+            Don't enter my brain with epilepsy, it's a disco
+            Inevitably you enter the depths of my diss throw
+            Chuck a face punch, oh, forgot you just misthrow
+            I bet your diet makes you diarrhea and piss mold
+            """)
+
+
+if __name__ == "__main__":
+    introduce()
